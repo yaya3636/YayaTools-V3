e3b8b6c8a8dc36ee9f599a0029e7194b7f8aa46c
Yaya <65079081+yaya3636@users.noreply.github.com>
2024-02-16 06:39:45 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,7 +1,7 @@
 ModuleLoader = dofile(global:getCurrentDirectory() .. [[Sandbox\YayaToolsV3\Module\ModuleLoader.lua]])(1)
 Loggerr = ModuleLoader:load("logger")(1)
 Utils = ModuleLoader:load("Utils")()
-Pushbullet = ModuleLoader:load("Pushbullet")("")
+Pushbullet = ModuleLoader:load("Pushbullet")("TokenAPIIci")
 MyJson = ModuleLoader:load("MyJson")()
 
 function move()
